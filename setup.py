659d9897a6e06938ab84d37c2860565cf4af94c8
Alberto Donato <alberto.donato@gmail.com>
2017-03-07 12:29:56 +0000

    Fix setup.py again

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ config = {
     'include_package_data': True,
     'entry_points': {'console_scripts': [
         'query-exporter = query_exporter.main:script']},
-    'test_suite': 'query-exporter',
+    'test_suite': 'query_exporter',
     'install_requires': [
         'aiohttp',
         'aiopg',
